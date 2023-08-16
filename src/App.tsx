16f4282f92cb1eef78cd23136ece2b283fdee77c
samuel Panzera <samuel.chagas@sga.pucminas.br>
2023-08-15 21:14:21 -0300

    Configuracoes de eslint e outras

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import { useState } from "react"
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  const incrementCount = () => {
+    return setCount((prevState) => prevState + 1)
+  }
+
+  const decrementCount = () => {
+    return setCount(count - 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{count}</h1>
+      <button onClick={incrementCount}>Increment</button>
+      <button onClick={decrementCount}>Decrementment</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
